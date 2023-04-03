cffa0c2c831aa39ae51467511141a17c34730e1b
Sohamk0301 <100216902+Sohamk0301@users.noreply.github.com>
2023-04-03 13:00:30 +0530

    1 file added

---
+++ src/app/formdata.ts
@@ -0,0 +1,11 @@
+export class Formdata {
+  constructor(
+    public name1:string,
+    public phone1:number,
+    public email1:string,
+    public schoolname1:string,
+    public programmename1:string,
+    public brochure:boolean
+  ){}
+  
+}
